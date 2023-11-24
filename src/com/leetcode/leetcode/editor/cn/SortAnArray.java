024a07e7a0bc050f4931e16d95d6ac44cb1d40e4
Lh <lihao1017@qq.com>
2023-11-24 11:22:24 +0800

    radix

--- src/com/leetcode/leetcode/editor/cn/SortAnArray.java
+++ src/com/leetcode/leetcode/editor/cn/SortAnArray.java
@@ -178,85 +178,162 @@ package com.leetcode.leetcode.editor.cn;
 //    }
 //}
 
-// 堆排序
+//// 堆排序
+//class Solution {
+//    public static int[] sortArray(int[] nums) {
+//        if (nums.length > 1) {
+//            // heapSort1为从顶到底建堆然后排序
+//            // heapSort2为从底到顶建堆然后排序
+//            // 用哪个都可以
+//            // heapSort1(nums);
+//            heapSort2(nums);
+//        }
+//        return nums;
+//    }
+//
+//    // i位置的数，向上调整大根堆
+//    // arr[i] = x，x是新来的！往上看，直到不比父亲大，或者来到0位置(顶)
+//    public static void heapInsert(int[] arr, int i) {
+//        // 新来的数与父亲位置的数作比较，直到不比父亲位置的数字大
+//        while (arr[i] > arr[(i - 1) / 2]) {
+//            // 如果大于父亲位置的数，则交换
+//            swap(arr, i, (i - 1) / 2);
+//            // 自己来到父亲位置，重复这个过程
+//            i = (i - 1) / 2;
+//        }
+//    }
+//
+//    // i位置的数，变小了，又想维持大根堆结构
+//    // 向下调整大根堆
+//    // 当前堆的大小为size
+//    public static void heapify(int[] arr, int i, int size) {
+//        int l = i * 2 + 1;
+//        while (l < size) {
+//            // 有左孩子，l
+//            // 右孩子，l+1
+//            // 评选，最强的孩子，是哪个下标的孩子
+//            int best = l + 1 < size && arr[l + 1] > arr[l] ? l + 1 : l;
+//            // 上面已经评选了最强的孩子，接下来，当前的数和最强的孩子之间，最强下标是谁
+//            best = arr[best] > arr[i] ? best : i;
+//            if (best == i) {
+//                break;
+//            }
+//            swap(arr, best, i);
+//            i = best;
+//            l = i * 2 + 1;
+//        }
+//    }
+//
+//    public static void swap(int[] arr, int i, int j) {
+//        int tmp = arr[i];
+//        arr[i] = arr[j];
+//        arr[j] = tmp;
+//    }
+//
+//    // 从顶到底建立大根堆，O(n * logn)
+//    // 依次弹出堆内最大值并排好序，O(n * logn)
+//    // 整体时间复杂度O(n * logn)
+//    public static void heapSort1(int[] arr) {
+//        int n = arr.length;
+//        for (int i = 0; i < n; i++) {
+//            heapInsert(arr, i);
+//        }
+//        int size = n;
+//        while (size > 1) {
+//            swap(arr, 0, --size);
+//            heapify(arr, 0, size);
+//        }
+//    }
+//
+//    // 从底到顶建立大根堆，O(n)
+//    // 依次弹出堆内最大值并排好序，O(n * logn)
+//    // 整体时间复杂度O(n * logn)
+//    public static void heapSort2(int[] arr) {
+//        int n = arr.length;
+//        for (int i = n - 1; i >= 0; i--) {
+//            heapify(arr, i, n);
+//        }
+//        int size = n;
+//        while (size > 1) {
+//            swap(arr, 0, --size);
+//            heapify(arr, 0, size);
+//        }
+//    }
+//}
+
+import java.util.Arrays;
+
+// 基数排序
 class Solution {
-    public static int[] sortArray(int[] nums) {
-        if (nums.length > 1) {
-            // heapSort1为从顶到底建堆然后排序
-            // heapSort2为从底到顶建堆然后排序
-            // 用哪个都可以
-            // heapSort1(nums);
-            heapSort2(nums);
-        }
-        return nums;
-    }
+    // 可以设置进制，不一定10进制，随便设置，跟循环次数有关系
+    public static int BASE = 1000;
 
-    // i位置的数，向上调整大根堆
-    // arr[i] = x，x是新来的！往上看，直到不比父亲大，或者来到0位置(顶)
-    public static void heapInsert(int[] arr, int i) {
-        // 新来的数与父亲位置的数作比较，直到不比父亲位置的数字大
-        while (arr[i] > arr[(i - 1) / 2]) {
-            // 如果大于父亲位置的数，则交换
-            swap(arr, i, (i - 1) / 2);
-            // 自己来到父亲位置，重复这个过程
-            i = (i - 1) / 2;
-        }
-    }
+    public static int MAXN = 50001;
 
-    // i位置的数，变小了，又想维持大根堆结构
-    // 向下调整大根堆
-    // 当前堆的大小为size
-    public static void heapify(int[] arr, int i, int size) {
-        int l = i * 2 + 1;
-        while (l < size) {
-            // 有左孩子，l
-            // 右孩子，l+1
-            // 评选，最强的孩子，是哪个下标的孩子
-            int best = l + 1 < size && arr[l + 1] > arr[l] ? l + 1 : l;
-            // 上面已经评选了最强的孩子，接下来，当前的数和最强的孩子之间，最强下标是谁
-            best = arr[best] > arr[i] ? best : i;
-            if (best == i) {
-                break;
+    public static int[] help = new int[MAXN];
+
+    public static int[] cnts = new int[BASE];
+
+    public static int[] sortArray(int[] arr) {
+        if (arr.length > 1) {
+            // 如果会溢出，那么要改用long类型数组来排序
+            int n = arr.length;
+            // 找到数组中的最小值
+            int min = arr[0];
+            for (int i = 1; i < n; i++) {
+                min = Math.min(min, arr[i]);
             }
-            swap(arr, best, i);
-            i = best;
-            l = i * 2 + 1;
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                // 数组中的每个数字，减去数组中的最小值，就把arr转成了非负数组
+                arr[i] -= min;
+                // 记录数组中的最大值
+                max = Math.max(max, arr[i]);
+            }
+            // 根据最大值在BASE进制下的位数，决定基数排序做多少轮
+            radixSort(arr, n, bits(max));
+            // 数组中所有数都减去了最小值，所以最后不要忘了还原
+            for (int i = 0; i < n; i++) {
+                arr[i] += min;
+            }
         }
+        return arr;
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+    // 返回number在BASE进制下有几位
+    public static int bits(int number) {
+        int ans = 0;
+        while (number > 0) {
+            ans++;
+            number /= BASE;
+        }
+        return ans;
     }
 
-    // 从顶到底建立大根堆，O(n * logn)
-    // 依次弹出堆内最大值并排好序，O(n * logn)
-    // 整体时间复杂度O(n * logn)
-    public static void heapSort1(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++) {
-            heapInsert(arr, i);
-        }
-        int size = n;
-        while (size > 1) {
-            swap(arr, 0, --size);
-            heapify(arr, 0, size);
-        }
-    }
-
-    // 从底到顶建立大根堆，O(n)
-    // 依次弹出堆内最大值并排好序，O(n * logn)
-    // 整体时间复杂度O(n * logn)
-    public static void heapSort2(int[] arr) {
-        int n = arr.length;
-        for (int i = n - 1; i >= 0; i--) {
-            heapify(arr, i, n);
-        }
-        int size = n;
-        while (size > 1) {
-            swap(arr, 0, --size);
-            heapify(arr, 0, size);
+    // 基数排序核心代码
+    // arr内要保证没有负数
+    // n是arr的长度
+    // bits是arr中最大值在BASE进制下有几位
+    public static void radixSort(int[] arr, int n, int bits) {
+        // 理解的时候可以假设BASE = 10
+        for (int offset = 1; bits > 0; offset *= BASE, bits--) {
+            Arrays.fill(cnts, 0);
+            for (int i = 0; i < n; i++) {
+                // 数字提取某一位的技巧
+                cnts[(arr[i] / offset) % BASE]++;
+            }
+            // 处理成前缀次数累加的形式
+            for (int i = 1; i < BASE; i++) {
+                cnts[i] = cnts[i] + cnts[i - 1];
+            }
+            for (int i = n - 1; i >= 0; i--) {
+                // 前缀数量分区的技巧
+                // 数字提取某一位的技巧
+                help[--cnts[(arr[i] / offset) % BASE]] = arr[i];
+            }
+            for (int i = 0; i < n; i++) {
+                arr[i] = help[i];
+            }
         }
     }
 }
